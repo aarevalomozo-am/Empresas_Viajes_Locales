8e731fb8b391e00fb4b2235ee52898246dcce5c7
OlivVSC <o.j.g.carrillo12@gmail.com>
2025-12-07 11:20:05 -0500

    Primer Commit: Interfaz principal

---
+++ Parcial_Final/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Parical_Final
+{
+    class Parcial_Final
+    {
+        static void Main()
+        {   
+            //Inicio del programa
+            Console.Clear();
+            Console.WriteLine("Bienvenido al sistema de gestión de transporte \nPresione cualquier tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+            //Menu principal
+            Console.WriteLine("Menu principal");
+            Console.WriteLine("1. Administración de choferes");
+            Console.WriteLine("2. Administración de rutas");
+            Console.WriteLine("3. Administración de puestos");
+            Console.WriteLine("4. Reiniciar sistema");
+            Console.WriteLine("5. Salir");
+            Console.Write("Seleccione una opción: ");
+
+        }
+
+    }
+}
