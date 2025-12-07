320c6ee16c16606830a928160b5305deafbd174d
OlivVSC <o.j.g.carrillo12@gmail.com>
2025-12-07 11:28:48 -0500

    Segund Commit: Agregar respuesta a la opcion establecida (no se ha realizado las funciones correspondientes), y en caso la opcion este fuera del rango imprimir error.

--- Parcial_Final/Program.cs
+++ Parcial_Final/Program.cs
@@ -5,21 +5,70 @@ namespace Parical_Final
     {
         static void Main()
         {   
+            //Variables
+            int opcion=0;
             //Inicio del programa
             Console.Clear();
             Console.WriteLine("Bienvenido al sistema de gestión de transporte \nPresione cualquier tecla para continuar...");
             Console.ReadKey();
-            Console.Clear();
             //Menu principal
-            Console.WriteLine("Menu principal");
-            Console.WriteLine("1. Administración de choferes");
-            Console.WriteLine("2. Administración de rutas");
-            Console.WriteLine("3. Administración de puestos");
-            Console.WriteLine("4. Reiniciar sistema");
-            Console.WriteLine("5. Salir");
-            Console.Write("Seleccione una opción: ");
+            while (opcion!=5 )
+            {
+                Console.Clear();
+                Console.WriteLine("Menu principal");
+                Console.WriteLine("1. Administración de choferes");
+                Console.WriteLine("2. Administración de rutas");
+                Console.WriteLine("3. Administración de puestos");
+                Console.WriteLine("4. Reiniciar sistema");
+                Console.WriteLine("5. Salir");
+                Console.Write("Seleccione una opción: ");
+                while(!int.TryParse(Console.ReadLine(), out opcion) || opcion < 1 || opcion > 5)
+                {
+                    Console.Write("\nOpcion invalida \nSeleccione una opcion del 1 al 5: ");
+                }
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("\nCargando administración de choferes");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.WriteLine(".");
+                        break;
+                        //Agregar las funcion.
+                    case 2:
+                        Console.Write("\nCargando administración de rutas");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.WriteLine(".");
+                        break;
+                    case 3:
+                        Console.Write("\nCargando administración de puestos");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                        Console.WriteLine(".");
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("\nEl sistema se ha reiniciado correctamente.\nPresione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Gracias por usar el sistema de gestión de transporte. ¡Hasta luego!");
+                        break;
+                }
 
+            }
         }
-
     }
 }
--- Parcial_Final/obj/Debug/net10.0/Parcial_Final.AssemblyInfo.cs
+++ Parcial_Final/obj/Debug/net10.0/Parcial_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parcial_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e731fb8b391e00fb4b2235ee52898246dcce5c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parcial_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parcial_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
